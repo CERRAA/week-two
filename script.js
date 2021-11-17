f584b6249ee0b2c12b3371571930ac9a96dfa9e7
CERRAA <cerraangie113@gmail.com>
2021-11-17 11:10:00 +0300

    start html

---
+++ script.js
@@ -0,0 +1,25 @@
+// const submitbutton = document.getElementById('get-name');
+// const date = document.getElementById('date')
+// const male = document.getElementById('varMale').value
+// const female = document.getElementById("varFemale").value
+
+// var days=['sunday','monday','tuesday','wednsday','thursday','friday','saturday','sunday'];
+// var male=['kwasi','kwadwo','kwabena','kwaku','Yaw','kofi','kwame'];
+// var female=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+
+
+
+
+// submitbutton.addEventListener('click', function (e) {
+
+//     const finalDate = new Date(date.value)
+//     e.preventDefault()
+
+//     console.log(finalDate.getDay());
+//     console.log(male);
+//     console.log(female);
+
+// })
+
+
+function 
